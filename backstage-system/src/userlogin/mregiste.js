2ba1344867e7bfea9b71adc3b0d196f28b9198f3
zl <1178822181@qq.com>
2019-10-22 16:07:54 +0800

    aaaa

--- backstage-system/src/userlogin/mregiste.js
+++ backstage-system/src/userlogin/mregiste.js
@@ -1,13 +1,15 @@
-/*
- * @Author: your name
- * @Date: 2019-10-21 20:19:47
- * @LastEditTime: 2019-10-21 21:50:35
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: /c:\Users\qf\Desktop\aaaaaaaaaaaaa\backstage-system\src\userlogin\mregiste.js
- */
+
 import React from "react";
 import axios from "axios";
+import '../mcss/denglu.css';
+import {
+	BrowserRouter,
+	HashRouter,
+	Route,
+	Switch,
+	Link,
+	Redirect
+  } from "react-router-dom";
 export class Registe extends React.Component {
   btn() {
     console.log(this.username.value);
@@ -20,7 +22,8 @@ export class Registe extends React.Component {
     console.log(user);
     axios.post("/bsystem/registe", user).then(function(s) {
       console.log(s.data);
-    });
+	});
+	alert("注册成功,请直接点击登录")
   }
   btn2() {
     let user = {
@@ -28,39 +31,53 @@ export class Registe extends React.Component {
       pw: this.pw.value + ""
     };
     axios.post("/bsystem/login", user).then(function(s) {
-      console.log(s.data);
-    });
+		console.log('-------------');
+	  console.log(s.data.login_ok);
+	  if (s.data.login_ok) {
+		window.location.reload();
+		  
+	  }
+	});
+	
   }
 
   render() {
     return (
       <div>
-        <input
-          type="text"
-          ref={username => {
-            this.username = username;
-          }}
-        />
-        <input
-          type="text"
-          ref={pw => {
-            this.pw = pw;
-          }}
-        />
-        <button
-          onClick={() => {
-            this.btn();
-          }}
-        >
-          注册
-        </button>
-        <button
-          onClick={() => {
-            this.btn2();
-          }}
-        >
-          登录
-        </button>
+
+				<div className="box">
+					<h1>学员后台系统</h1>
+					
+					<input className="input1" placeholder="身份证号码/学号"
+					type="text"
+					ref={username => {
+						this.username = username;
+					}}
+					/>
+					<br/>
+					<input className="input2" placeholder="密码"
+					type="text"
+					ref={pw => {
+						this.pw = pw;
+					}}
+					/>
+					<br/>
+					<button className="b1"
+					onClick={() => {
+						this.btn();
+					}}
+					>
+					注册
+					</button>
+					<button className="b2"
+					onClick={() => {
+						this.btn2();
+					}}
+					>
+					登录
+					</button>
+					
+				</div>
       </div>
     );
   }
